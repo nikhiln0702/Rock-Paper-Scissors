35311d90c2c957982a206fb312ef532a69ca6a20
nikhil_n <141673885+nikhiln0702@users.noreply.github.com>
2024-12-26 16:12:33 +0000

    completed the get computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-function getComputerChoice()
+function getComputerChoice(){
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+      }
+    let num=getRandomInt(3);
+    let choice;
+    if(num==0){
+        choice="rock";
+    }
+    else if(num==1){
+        choice="paper";
+    }
+    else{
+        choice="scissors";
+    }
+    console.log(choice);
+    return choice;
+}
+getComputerChoice();
+function getHumanChoice(humanChoice){
+    humanChoice=humanChoice.toLowerCase();
+    if(humanChoice=="rock"){
+        
+    }
+}
